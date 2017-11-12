a6595e03626c8d95f249ff78035cdc707dbfb980
chemceem <chemceem@gmail.com>
2017-11-12 18:28:39 -0500

    Solution for Time Complexity: Primality

---
+++ src/hackerrank/algorithms/BirthdayCakeCandles.java
@@ -0,0 +1,25 @@
+package hackerrank.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BirthdayCakeCandles {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
