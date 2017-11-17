6272dbef2616e19ab84363c861e40d337afbe3f1
chemceem <chemceem@gmail.com>
2017-11-16 23:57:24 -0500

    Solution for Davis's staircase

---
+++ src/hackerrank/Techniques_Concepts/Recursion_Davis_Staircase.java
@@ -0,0 +1,36 @@
+package hackerrank.Techniques_Concepts;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Solution to https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem
+ */
+public class Recursion_Davis_Staircase {
+
+    private static HashMap<Integer, Integer> steps = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        for(int a0 = 0; a0 < s; a0++){
+            int n = in.nextInt();
+            System.out.println(staircase(n));
+        }
+
+        in.close();
+    }
+
+    private static int staircase(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (steps.containsKey(n)) {
+            return steps.get(n);
+        }
+        int ways = staircase(n - 1) + staircase(n - 2) + staircase(n - 3);
+        steps.put(n, ways);
+        return ways;
+    }
+
+}
